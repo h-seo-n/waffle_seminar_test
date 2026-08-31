dbc8a8c4e390c3351c929bce61d3961ef12b530b
Seoyeon HEO <best3487@snu.ac.kr>
2026-08-31 19:18:28 +0900

    chore: set base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/waffle_seminar_test/",
   plugins: [react()],
   test: {
     environment: "jsdom",
